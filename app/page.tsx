4a20f0009cffb20b346912d27e8526cb75d57be2
Hiralvala563 <hiralvala@lampros.tech>
2025-11-04 23:28:17 +0530

    Started to  make tech stack page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import HomePage from "@/components/HomePage/HomePage";
 import MyJourney from "@/components/MyJourney/Myjourney";
 import MyProjects from "@/components/MyProjects/MyProjects";
+import Techstack from "@/components/TechStack/Techstack";
 import Image from "next/image";
 
 export default function Home() {
@@ -8,6 +9,7 @@ export default function Home() {
     <>
     <HomePage />
     <MyJourney/>
+    <Techstack/>
     <MyProjects/>
     </>
   );
---
+++ components/TechStack/Techstack.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Techstack = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Techstack
